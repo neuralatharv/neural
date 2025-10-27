1346100f2c813b297b384e4d7a65c7a2aaebfde6
Imuric <88447716+Imuric@users.noreply.github.com>
2025-10-27 15:27:03 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,11 +2,11 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Prathamesh Patil",
+  initials: "PP",
+  url: "https://bento.me/uxbypratham",
+  location: "Pune, IN",
+  locationLink: "https://www.google.com/maps/place/pune",
   description:
     "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
   summary:
