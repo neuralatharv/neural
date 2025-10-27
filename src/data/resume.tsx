58a10f52408bd7d4222068f5f329c3ee26343f07
Imuric <88447716+Imuric@users.noreply.github.com>
2025-10-27 18:16:57 +0530

    Update name in resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Pratham",
+  name: "Prathamamesh Patil",
   initials: "PP",
   url: "https://bento.me/uxbypratham",
   location: "Pune, IN",
