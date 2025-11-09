a82df366de13e9c9a1a42a4709f8feea011b1cfd
neuralatharv <neural.atharv@gmail.com>
2025-11-09 23:21:56 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,7 +11,7 @@ export const DATA = {
     "AI and Data Science Student",
   summary:
     "Hey this is Atharv Kathe, Pursuing BTech Degree From ISBM College Of Engineering. ",
-  avatarUrl:"Atharv.png",
+  avatarUrl:"Atharva.png",
 skills: [
   "Vibe Coding",
   "Data Visualizer",
