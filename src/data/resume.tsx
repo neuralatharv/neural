ba2c5a4cc32ffd6ac791202803125209999cc696
neuralatharv <neural.atharv@gmail.com>
2025-11-09 21:05:21 +0530

    Update ERROR SOLVE.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -3,6 +3,7 @@ import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
   name: "Atharv Kathe",
+  initials:"AK",
   logoUrl: "Atharv.png",
   url: "https://bento.me/uxbypratham",
   location: "Pune, IN",
